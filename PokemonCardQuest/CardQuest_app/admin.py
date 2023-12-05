6c6c3892e6ea506a18eb5f13d65fd28d86a4c68f
Aceuuu08 <141605070+Aceuuu08@users.noreply.github.com>
2023-12-05 13:35:51 +0800

    major commit back to square 1

---
+++ PokemonCardQuest/CardQuest_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import PokemonCard, Trainer, Collection
+# Register your models here.
+admin.site.register(PokemonCard)
+admin.site.register(Trainer)
+admin.site.register(Collection)
