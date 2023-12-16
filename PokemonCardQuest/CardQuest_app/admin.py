d516ad652407bf4131ddc4bddc5e1c9a36223fd6
Aceuuu08 <141605070+Aceuuu08@users.noreply.github.com>
2023-12-16 21:59:19 +0800

    Update admin.py

--- PokemonCardQuest/CardQuest_app/admin.py
+++ PokemonCardQuest/CardQuest_app/admin.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
 from .models import PokemonCard, Trainer, Collection
 # Register your models here.
-admin.site.register(PokemonCard)
-admin.site.register(Trainer)
-admin.site.register(Collection)
 
-@admin.register(PokemonCard)
-class PokemonAdmin(admin.ModelAdmin):
-    list_display = ("name", "rarity", "hp", "card_type", "attack", "description", "weakness", "card_number", "release_date", "evolution_stage", "abilities", "created_at", "updated_at")
-    search_fields = ("name",)
+class BaseModelAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'updated_at')
+
+class TrainerAdmin(BaseModelAdmin):
+    list_display = ('name', 'birthdate', 'location', 'email', 'created_at', 'updated_at')
     
-@admin.register(Trainer)
-class TrainerAdmin(admin.ModelAdmin):
-    list_display = ("name", "location", "email", "birthdate", "created_at", "updated_at", )
-    search_fields = ("name",)
-    
-@admin.register(Collection)
-class CollectionAdmin(admin.ModelAdmin):
-    list_display = ("trainer", "card", "collection_date", "created_at", "updated_at", )
-    search_fields = ("name",)
+class PokemonCardAdmin(BaseModelAdmin):
+    list_display = (
+        'name', 'rarity', 'hp', 'card_type', 'attack', 'description', 'weakness',
+        'card_number', 'release_date', 'evolution_stage', 'abilities', 'created_at', 'updated_at'
+    )
+
+class CollectionAdmin(BaseModelAdmin):
+    list_display = ('card', 'trainer', 'collection_date', 'created_at', 'updated_at')
+
+
+
+admin.site.register(PokemonCard, PokemonCardAdmin)
+admin.site.register(Trainer, TrainerAdmin)
+admin.site.register(Collection, CollectionAdmin)
