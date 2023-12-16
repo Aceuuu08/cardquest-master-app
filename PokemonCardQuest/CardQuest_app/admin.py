6d5cabb37da72809548212d1d9ee9298f6ad5b04
Aceuuu08 <141605070+Aceuuu08@users.noreply.github.com>
2023-12-16 16:53:47 +0800

    revert admin.py

--- PokemonCardQuest/CardQuest_app/admin.py
+++ PokemonCardQuest/CardQuest_app/admin.py
@@ -4,16 +4,3 @@ from .models import PokemonCard, Trainer, Collection
 admin.site.register(PokemonCard)
 admin.site.register(Trainer)
 admin.site.register(Collection)
-
-
-class PokemonAdmin(admin.ModelAdmin):
-    list_display = ("name", "rarity", "hp", "card_type", "attack", "description", "weakness", "card_number", "release_date", "evolution_stage", "abilities", "created_at", "updated_at")
-    search_fields = ("name",)
-
-class TrainerAdmin(admin.ModelAdmin):
-    list_display = ("name", "location", "email", "birthdate", "created_at", "updated_at", )
-    search_fields = ("name",)
-
-class CollectionAdmin(admin.ModelAdmin):
-    list_display = ("trainer", "card", "collection_date", "created_at", "updated_at", )
-    search_fields = ("name",)
